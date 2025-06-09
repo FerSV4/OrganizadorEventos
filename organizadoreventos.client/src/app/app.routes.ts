a862e28d221a90ab32e2a7d3797651cea39b99bb
FerSV4 <ferandressc@hotmail.com>
2025-06-08 23:42:20 -0400

    Modulo de registro
    Implementacion de modulo de registro

--- organizadoreventos.client/src/app/app.routes.ts
+++ organizadoreventos.client/src/app/app.routes.ts
@@ -1,39 +1,18 @@
-
 import { Routes } from '@angular/router';
-
-
+    
 import { EventoFormComponent } from './eventos/evento-form/evento-form.component';
 import { EventoListComponent } from './eventos/evento-list/evento-list.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
+  { path: 'eventos', component: EventoListComponent },
+  { path: 'eventos/nuevo', component: EventoFormComponent },
+  { path: 'login', component: LoginComponent },
 
+  { path: 'register', component: RegisterComponent },
 
-  { 
-    path: 'eventos', 
-    component: EventoListComponent 
-  },
+  { path: '', redirectTo: '/eventos', pathMatch: 'full' },
 
-  { 
-    path: 'eventos/nuevo', 
-    component: EventoFormComponent 
-  },
-  
-
-  { 
-    path: 'login', 
-    component: LoginComponent 
-  },
-
-  { 
-    path: '', 
-    redirectTo: '/eventos', 
-    pathMatch: 'full' 
-  },
-
-  { 
-    path: '**', 
-    redirectTo: '/eventos' 
-  }
+  { path: '**', redirectTo: '/eventos' }
 ];
